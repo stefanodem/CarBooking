631a183704d7d9ccdaa0973e56620c35462d6de6
stdemicheli <stdemicheli@gmail.com>
2019-01-18 14:00:54 +0100

    Adds a network data loader protocol and extension for loading data from network.

--- CarBooking/CarBooking/Service/NetworkDataLoader.swift
+++ CarBooking/CarBooking/Service/NetworkDataLoader.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+/**
+ A network data loader protocol and extension for loading data from network.
+ */
+
+protocol NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping(Data?, URLResponse?,  Error?) -> Void)
+    func loadData(from url: URL, completion: @escaping(Data?, URLResponse?, Error?) -> Void)
+}
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataTask(with: request) { (data, res, error) in
+            completion(data, res, error)
+        }.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataTask(with: url) { (data, res, error) in
+            completion(data, res, error)
+        }.resume()
+    }
+}
