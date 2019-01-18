f2e33e429930b7f34eb918350f3c2b5c33d0c41f
stdemicheli <stdemicheli@gmail.com>
2019-01-18 14:02:34 +0100

    Adds an API service for handling network requests for vehicles.

--- CarBooking/CarBooking/Service/APIVehicleService.swift
+++ CarBooking/CarBooking/Service/APIVehicleService.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+/**
+ The API service handling network requests for vehicles.
+ */
+
+class APIVehicleService: APIBaseService {
+    
+    // MARK: - Properties
+    let baseUrl = URL(string: "http://job-applicants-dummy-api.kupferwerk.net.s3.amazonaws.com/api/")!
+    
+    // MARK: - Network requests
+    func fetchVehicles(completion: @escaping (Response<VehicleRepresentation>) -> ()) {
+        let url = self.url(with: baseUrl, pathComponents: ["cars"])
+        fetch(from: url, completion: completion)
+    }
+    
+}
