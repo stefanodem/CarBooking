70ef3048f09592519097b79c6843c59379ab7b7a
stdemicheli <stdemicheli@gmail.com>
2019-01-18 11:42:33 +0100

    Sets up folder structure.

---
+++ CarBooking/CarBooking/Resources/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  CarBooking
+//
+//  Created by De MicheliStefano on 18.01.19.
+//  Copyright © 2019 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Instantiate Client to be injected into root VC
+        // Setup root VC
+        
+        return true
+    }
+
+}
